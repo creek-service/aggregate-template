0a9f17342311e49dedbf58742ccda6199545c6a7
Andy Coates <8012398+big-andy-coates@users.noreply.github.com>
2023-03-18 17:22:06 +0000

    Expose aggregate descriptor (#177)
    Previously, the aggregate descriptor existed, but was not exposed as `provides ComponentDescriptor`.

--- api/src/main/java/module-info.java
+++ api/src/main/java/module-info.java
@@ -1,7 +1,13 @@
+import org.acme.example.api.ExampleAggregateDescriptor;
+import org.creekservice.api.platform.metadata.ComponentDescriptor;
+
 module example.mod.api {
     requires transitive creek.kafka.metadata;
 
     exports org.acme.example.api;
     exports org.acme.example.internal to
             example.mod.services;
+
+    provides ComponentDescriptor with
+            ExampleAggregateDescriptor;
 }
